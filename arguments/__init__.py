f0c854fcdacdbedcaac2c39065585fa792c44844
RodLoLo15 <rod_lopez_loja_97@hotmail.es>
2025-06-28 19:13:59 -0500

    Corrección a ModelParams

--- arguments/__init__.py
+++ arguments/__init__.py
@@ -46,10 +46,9 @@ class ParamGroup:
 
 class ModelParams(ParamGroup): 
     def __init__(self, parser, sentinel=False):
-        parser.add_argument('--model_path', type=str, default="./output", help="Ruta de salida del modelo")
         self.sh_degree = 3
         self._source_path = ""
-        self._model_path = ""
+        self._model_path = "./output"  # valor por defecto
         self._images = "images"
         self._depths = ""
         self._resolution = -1
